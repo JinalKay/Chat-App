dc9ef83b686df38a094dba7c8a7f8f956406bb8b
JinalKay <142990120+JinalKay@users.noreply.github.com>
2025-01-21 23:05:35 +0530

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -115,9 +115,10 @@ WSGI_APPLICATION = 'a_core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/JinalKay/Chat-App/db.sqlite3',
     }
 }
+
 
 
 # Password validation
